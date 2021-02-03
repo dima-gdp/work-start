95e1474fe4fc0f59764aa67fb238716a7a064997
iv-tsim <iv-tsim@yandex.ru>
2021-02-03 14:48:13 +0300

    added new modal, checked.svg

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,6 +1,62 @@
 $(document).ready(function () {
 	// objectFitImages();
 
+	// Alertify
+	// alertify.set('notifier', 'position', 'bottom-right');
+	// alertify.set('notifier', 'delay', 10);
+
+	// document.addEventListener('wpcf7mailsent', function (event) {
+	// 	alertify.success(event.detail.apiResponse.message)
+	// }, false);
+
+	// document.addEventListener('wpcf7invalid', function (event) {
+	// 	alertify.warning(event.detail.apiResponse.message);
+	// }, false);
+
+	// document.addEventListener('wpcf7mailfailed', function (event) {
+	// 	alertify.error(event.detail.apiResponse.message);
+	// }, false);
+
+	// $(document).on('click', '.wpcf7-submit', function (e) {
+	// 	if ($('.ajax-loader').hasClass('is-active')) {
+	// 		e.preventDefault();
+	// 		return false;
+	// 	}
+	// });
+
+	// Swiper
+	// const slider_pag = new Swiper('.pag-hero__slider', {
+	// 	slidesPerView: 'auto',
+	// 	spaceBetween: 45,
+	// 	loop: false,
+	// 	loopedSlides: 1,
+	// 	slideToClickedSlide: true,
+	// 	breakpoints: {
+	// 		320: {
+	// 			spaceBetween: 15,
+	// 		},
+	// 		374: {
+	// 			spaceBetween: 25,
+	// 		},
+	// 		577: {
+	// 			spaceBetween: 35,
+	// 		},
+	// 		625: {
+	// 			spaceBetween: 40,
+	// 		},
+	// 		769: {
+	// 			spaceBetween: 15,
+	// 		},
+	// 		993: {
+	// 			spaceBetween: 30,
+	// 		},
+	// 		1440: {
+	// 			spaceBetween: 45,
+	// 		},
+	// 	}
+	// });
+
+
 	// Fancy-box
 	// $(".to-modal").on('click', function () {
 	// 	$.fancybox.open({
